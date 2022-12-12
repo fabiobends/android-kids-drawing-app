46a7247dfa5fd3842c24349503937c653484b555
Fábio Mendes <fabiobends@gmail.com>
2022-12-12 17:16:01 -0300

    feat: handle permission to get images from gallery
    Handle permission to read external storage, for instance images
    from gallery.

--- app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
@@ -1,26 +1,67 @@
 package com.example.kidsdrawingapp
 
+import android.Manifest
+import android.app.AlertDialog
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
   private var drawingView: DrawingView? = null
   private var brushButton: ImageButton? = null
+  private var galleryButton: ImageButton? = null
   private var brushDialog: Dialog? = null
   private var brushSizeButtons = ArrayList<ImageButton>()
   private var currentPaintButton: ImageButton? = null
+
+  private val requestPermission: ActivityResultLauncher<Array<String>> =
+    permissionResultLauncher()
+
+  private fun permissionResultLauncher() =
+    registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permission ->
+      permission.entries.forEach {
+        val permissionName = it.key
+        val isGranted = it.value
+        if (isGranted) {
+          displayToast("Permission granted now you can read the storage files.")
+        } else {
+          checkPermissionAndDisplayDeniedToast(permissionName)
+        }
+      }
+    }
+
+  private fun displayToast(text: String) {
+    Toast.makeText(
+      this,
+      text,
+      Toast.LENGTH_LONG
+    ).show()
+  }
+
+  private fun checkPermissionAndDisplayDeniedToast(permissionName: String) {
+    if (permissionName == Manifest.permission.READ_EXTERNAL_STORAGE) {
+      displayToast("Oops you just denied the permission.")
+    }
+  }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
     drawingView = findViewById(R.id.drawing_view)
     brushButton = findViewById(R.id.brush)
+    galleryButton = findViewById(R.id.gallery)
     brushButton?.setOnClickListener {
       showBrushSizeChooserDialog()
+    }
+    galleryButton?.setOnClickListener {
+      requestStoragePermission()
     }
   }
 
@@ -68,6 +109,18 @@ class MainActivity : AppCompatActivity() {
     }
   }
 
+  private fun requestStoragePermission() {
+    if (ActivityCompat.shouldShowRequestPermissionRationale(
+        this,
+        Manifest.permission.READ_EXTERNAL_STORAGE
+      )
+    ) {
+      showRationaleDialog("Kids Drawing App", "The app needs to access your external storage")
+    } else {
+      requestPermission.launch(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE))
+    }
+  }
+
   fun onPaintClick(view: View) {
     if (view !== currentPaintButton) {
       val selectedPaintButton = view as ImageButton
@@ -100,5 +153,15 @@ class MainActivity : AppCompatActivity() {
         R.drawable.pallet_regular
       )
     )
+  }
+
+  private fun showRationaleDialog(title: String, message: String) {
+    val builder = AlertDialog.Builder(this)
+    builder.setTitle(title)
+      .setMessage(message)
+      .setPositiveButton("Cancel") { dialog, _ ->
+        dialog.dismiss()
+      }
+    builder.create().show()
   }
 }
